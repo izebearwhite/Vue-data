f12cbb1771e37a2f8b713a8b2d10bb909dd5d764
csmaejo <izebearwhite@gmail.com>
2021-12-26 19:17:38 +0700

    first commit

---
+++ ice.js
@@ -0,0 +1,16 @@
+var example = new Vue({
+    el: "#example",
+    data:{
+        name:"Activity",
+        p:"pattaravadee junthamanee",
+        l:"I'M ICECREAM",
+        X:" work",
+        V:"learning ",
+        B:"money ",
+       
+
+        
+
+    },
+    
+})
